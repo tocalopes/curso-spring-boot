1b4acf6953cdc6a5cfbd96a2af2fa0349af5b2f9
gabriel lopes <gabriellopes112009@gmail.com>
2020-02-13 16:38:17 -0300

    Start of Spring Boot Project for the Java completo 2020 from Udemy

---
+++ src/test/java/com/lopes/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopes.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
