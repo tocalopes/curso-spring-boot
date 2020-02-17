a9fb5a98d8a792b1ceeda86b8d7b54652411c65c
gabriel lopes <gabriellopes112009@gmail.com>
2020-02-17 10:45:47 -0300

    h2 database, test profile, jpa

--- src/main/java/com/lopes/curso/entities/User.java
+++ src/main/java/com/lopes/curso/entities/User.java
@@ -2,11 +2,21 @@ package com.lopes.curso.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
 
 //Serializable é usado para trafegar dados na rede
+@Entity
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String phone;
