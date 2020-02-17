acd129f6d4270ce88f7ce714c8dc6345d35f97e4
gabriel lopes <gabriellopes112009@gmail.com>
2020-02-17 16:47:47 -0300

    Service layer, component registration

--- src/main/java/com/lopes/curso/entities/User.java
+++ src/main/java/com/lopes/curso/entities/User.java
@@ -23,6 +23,10 @@ public class User implements Serializable {
 	private String email;
 	private String password;
 	
+	public User() {
+		
+	}
+	
 	public User(Long id, String name, String phone, String email, String password) {
 		super();
 		this.id = id;
--- src/main/java/com/lopes/curso/resources/UserResource.java
+++ src/main/java/com/lopes/curso/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.lopes.curso.resources;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lopes.curso.entities.User;
+import com.lopes.curso.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User (1L,"gabriel","gabriel@gmail.com","3123123","13123");
-		return ResponseEntity.ok().body(u); 
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
