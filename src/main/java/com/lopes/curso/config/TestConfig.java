9012b564cca349d5437adbb9ef83253c0a58b57e
gabriel lopes <gabriellopes112009@gmail.com>
2020-02-18 15:00:53 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/lopes/curso/config/TestConfig.java
+++ src/main/java/com/lopes/curso/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.lopes.curso.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.lopes.curso.entities.Order;
 import com.lopes.curso.entities.User;
+import com.lopes.curso.repositories.OrderRepository;
 import com.lopes.curso.repositories.UserRepository;
 
 @Configuration
@@ -18,14 +21,22 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+		
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		User u1 = new User(null,"Gabriel Lopes","gabriellopes112009@gmail.com","964408814","5322");
 		User u2 = new User(null,"Judite Lopes","judite_oliveira@hotmail.com","963448814","9076");
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
 		
+		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
 	
 	
---
+++ src/main/java/com/lopes/curso/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.lopes.curso.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lopes.curso.entities.Order;
+import com.lopes.curso.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
