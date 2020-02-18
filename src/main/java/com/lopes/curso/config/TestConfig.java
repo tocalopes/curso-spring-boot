24ea0dfa7b8c68b351cb20770574cc8ab2db2dfd
gabriel lopes <gabriellopes112009@gmail.com>
2020-02-18 16:23:50 -0300

    Product

--- src/main/java/com/lopes/curso/config/TestConfig.java
+++ src/main/java/com/lopes/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.lopes.curso.entities.Category;
 import com.lopes.curso.entities.Order;
+import com.lopes.curso.entities.Product;
 import com.lopes.curso.entities.User;
 import com.lopes.curso.entities.enums.OrderStatus;
 import com.lopes.curso.repositories.CategoryRepository;
 import com.lopes.curso.repositories.OrderRepository;
+import com.lopes.curso.repositories.ProductRepository;
 import com.lopes.curso.repositories.UserRepository;
 
 @Configuration
@@ -31,6 +33,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -38,6 +43,11 @@ public class TestConfig implements CommandLineRunner {
 		Category c2 = new Category(null, "Books");
 		Category c3 = new Category(null, "Computers"); 
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 		
 		
 		User u1 = new User(null,"Gabriel Lopes","gabriellopes112009@gmail.com","964408814","5322");
 		User u2 = new User(null,"Judite Lopes","judite_oliveira@hotmail.com","963448814","9076");
@@ -50,6 +60,7 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 	}
 	
---
+++ src/main/java/com/lopes/curso/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.lopes.curso.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lopes.curso.entities.Product;
+import com.lopes.curso.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
