e95770b11e73dee3847be2d407fa1cb1052d47a3
gabriel lopes <gabriellopes112009@gmail.com>
2020-02-19 12:16:17 -0300

    OrderItem, many-to-many association with extra attribute part 2

--- src/main/java/com/lopes/curso/config/TestConfig.java
+++ src/main/java/com/lopes/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.lopes.curso.entities.Category;
 import com.lopes.curso.entities.Order;
+import com.lopes.curso.entities.OrderItem;
 import com.lopes.curso.entities.Product;
 import com.lopes.curso.entities.User;
 import com.lopes.curso.entities.enums.OrderStatus;
 import com.lopes.curso.repositories.CategoryRepository;
+import com.lopes.curso.repositories.OrderItemRepository;
 import com.lopes.curso.repositories.OrderRepository;
 import com.lopes.curso.repositories.ProductRepository;
 import com.lopes.curso.repositories.UserRepository;
@@ -21,59 +23,65 @@ import com.lopes.curso.repositories.UserRepository;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
-	
-	//Autowired indica que existe uma dependecia
+
+	// Autowired indica que existe uma dependecia
 	@Autowired
 	private UserRepository userRepository;
 
-		
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Category c1 = new Category(null, "Electronics");
 		Category c2 = new Category(null, "Books");
-		Category c3 = new Category(null, "Computers"); 
-		
+		Category c3 = new Category(null, "Computers");
+
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 		
-		
-		categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
-		
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		p1.getCategories().add(c2);
 		p2.getCategories().add(c1);
 		p2.getCategories().add(c3);
 		p3.getCategories().add(c3);
 		p4.getCategories().add(c3);
 		p5.getCategories().add(c2);
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		User u1 = new User(null, "Gabriel Lopes", "gabriellopes112009@gmail.com", "964408814", "5322");
+		User u2 = new User(null, "Judite Lopes", "judite_oliveira@hotmail.com", "963448814", "9076");
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
-		
-		User u1 = new User(null,"Gabriel Lopes","gabriellopes112009@gmail.com","964408814","5322");
-		User u2 = new User(null,"Judite Lopes","judite_oliveira@hotmail.com","963448814","9076");
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1); 
-		
-		
-		userRepository.saveAll(Arrays.asList(u1,u2));
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 	}
-	
-	
-	
+
 }
---
+++ src/main/java/com/lopes/curso/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.lopes.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lopes.curso.entities.OrderItem;
+
+//A implementação da interface é feita por padrão pelo spring 
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	
+	
+}
